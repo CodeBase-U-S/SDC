619947f311a1d8c6f737013d3b80c70619a63154
Miguel Buenviaje <migbuen44@gmail.com>
2021-08-09 14:21:48 -0700

    Refactored q_and_a files to follow controller, model method

---
+++ server/controllers/q_and_a.js
@@ -0,0 +1,119 @@
+const db = require('../q_and_a_database/index.js');
+
+module.exports = {
+  getQuestions: (req, res) => {
+    let { product_id, count } = req.query;
+
+    if (!count) {
+      count = 4;
+    }
+    db.getQuestions(product_id, count, (err, result) => {
+      if (err) {
+        res.send('error');
+      } else {
+        res.send(result);
+      }
+    });
+  },
+  addQuestion: (req, res) => {
+    const { product_id, body, name, email } = req.body;
+
+    db.addQuestion(product_id, body, name, email, (err, results) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(results);
+      }
+    });
+  },
+  getAnswers: (req, res) => {
+    let { question_id } = req.params;
+    let { count } = req.query;
+
+    if (!count) {
+      count = 4;
+    }
+    db.getAnswers(question_id, count, (err, results) => {
+      if (err) {
+        res.send('error');
+      } else {
+        res.send(results);
+      }
+    });
+  },
+  addAnswer: (req, res) => {
+    const { question_id } = req.params;
+    let { body, date_written, name, email, photos } = req.body;
+    let dt = new Date();
+    dt = dt.getTime();
+    // console.log(dt);
+    if (!date_written) {
+      let dt = new Date();
+      dt = dt.getTime();
+      date_written = dt;
+    }
+    if (!Array.isArray(photos)) {
+      photos = [];
+    }
+    db.addAnswer(question_id, body, date_written, name, email, photos, (err, results) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(results);
+      }
+    });
+  },
+  updateQuestionHelpful: (req, res) => {
+    // console.log('req.body in helpful put: ', req.body);
+    let { question_id } = req.params;
+    if (!question_id) {
+      res.send('error');
+    }
+    db.addQuestionHelpful(question_id, (err, results) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(results);
+      }
+    });
+  },
+  updateAnswerHelpful: (req, res) => {
+    let { answer_id } = req.params;
+    if (!answer_id) {
+      res.send('error');
+    }
+    db.addAnswerHelpful(answer_id, (err, results) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(results);
+      }
+    });
+  },
+  reportQuestion: (req, res) => {
+    let { question_id } = req.params;
+    if (!question_id) {
+      res.send('error');
+    }
+    db.reportQuestion(question_id, (err, results) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(results);
+      }
+    });
+  },
+  reportAnswer: (req, res) => {
+    let { answer_id } = req.params;
+    if (!answer_id) {
+      res.send('error');
+    }
+    db.reportAnswer(answer_id, (err, results) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(results);
+      }
+    });
+  },
+};
