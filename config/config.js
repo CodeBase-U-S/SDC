b855f3c29095b4be791d36ad30c9779b74f446f2
Miguel Buenviaje <migbuen44@gmail.com>
2021-10-01 19:34:04 -0700

    Revert commit: 6a655ea

--- config/config.js
+++ config/config.js
@@ -1,10 +1,6 @@
 module.exports = {
   user: 'postgres',
-<<<<<<< HEAD
   host: '3.135.200.26',
-=======
-  host: '3.139.97.207',
->>>>>>> 22c9abebe38fb9753e9e8402ddebec69dca81af4
   database: 'postgres',
   password: 'password',
   port: 5432,
